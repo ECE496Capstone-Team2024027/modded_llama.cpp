6b1bc4727324c5be12c75cd579d192f41e6598c5
Angus Wu <wuangusjh@gmail.com>
2025-01-20 14:02:30 -0500

    cleaned up build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -1,7 +1,17 @@
 rm -rf build_offall
 mkdir build_offall
 cd build_offall
-cmake .. -DCMAKE_C_FLAGS="-mno-avx2 -mno-avx -mno-fma -mno-sse3 -mno-ssse3" -DCMAKE_CXX_FLAGS="-mno-avx2 -mno-avx -mno-fma -mno-sse3 -mno-ssse3" -DGGML_METAL=OFF -DGGML_BLAS=OFF -DGGML_LLAMAFILE=OFF -DGGML_COMPILER_SUPPORT_MATMUL_INT8=OFF -DGGML_COMPILER_SUPPORT_FP16_VECTOR_ARITHMETIC=OFF -DGGML_COMPILER_SUPPORT_DOTPROD=OFF -DGGML_NATIVE=OFF -DGGML_ACCELERATE=OFF -DGGML_SIMD=OFF
+cmake .. -DCMAKE_C_FLAGS="-mno-avx2 -mno-avx -mno-fma -mno-sse3 -mno-ssse3" \
+    -DCMAKE_CXX_FLAGS="-mno-avx2 -mno-avx -mno-fma -mno-sse3 -mno-ssse3" \
+    -DGGML_METAL=OFF \
+    -DGGML_BLAS=OFF \
+    -DGGML_LLAMAFILE=OFF \
+    -DGGML_COMPILER_SUPPORT_MATMUL_INT8=OFF \
+    -DGGML_COMPILER_SUPPORT_FP16_VECTOR_ARITHMETIC=OFF \
+    -DGGML_COMPILER_SUPPORT_DOTPROD=OFF \
+    -DGGML_NATIVE=OFF \
+    -DGGML_ACCELERATE=OFF \
+    -DGGML_SIMD=OFF
 make
 
 MODEL="./dir/to/model.gguf"
